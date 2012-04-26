c84d2a1db5978b91c5458cf4b3a1785178ae6fd5
Jose Gomez-Dans <jgomezdans@gmail.com>
2012-04-26 13:56:42 +0100

    Added a transformed function to linearise the model

--- prosail/__init__.py
+++ prosail/__init__.py
@@ -1,1 +1,22 @@
 from prosail import run_prosail
+
+def trans_prosail ( N, cab, car, cbrown, cw, cm, lai, lidfa, lidfb, psoil, \
+        hspot, tts, tto, psi ):
+    """A version of PROSAIL that uses transformed parameters to quasi-linearise
+    the   model. See http://dx.doi.org/10.1016/j.rse.2011.12.027"""
+    # Define the constants
+    slai = -2.0
+    skab = -100.0
+    skar = -100.0
+    skw =  -1./50.
+    skm =  -1./100.
+    # Transform the parameters to real units
+    xlai = slai * np.log ( lai )
+    xkab = skab * np.log ( cab )
+    xkar = skar * np.log ( car )
+    xkw = skw * np.log ( cw )
+    xdm = skm * np.log ( dm )
+    # Run the PROSAIL model
+    retval = run_prosail ( N, xkab, xkar, cbrown, xkw, xdm, xlai, \
+            lidfa, lidfb, psoil, hspot, tts, tto, psi )
+    return retval
