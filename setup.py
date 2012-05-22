7fc0f8d1ca21a2086ab9d64c8577a081a5f54cf3
Jose Gomez-Dans <jgomezdans@gmail.com>
2012-05-22 18:52:46 +0100

    Changed setup to have the right module name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('prosail',parent_package,top_path)
+    config = Configuration(parent_package,top_path)
     config.add_extension('prosail', ['prosail/dataSpec_P5B.f90', \
         "prosail/dladgen.f", "prosail/LIDF.f90", "prosail/MODULE_PRO4SAIL.f90", \
         "prosail/PRO4SAIL.f90", "prosail/prospect_5B.f90", \
