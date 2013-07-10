5fa1ca48022bc197b812c57f3b00ca762a6482f2
José Gómez-Dans <jgomezdans@gmail.com>
2013-07-10 13:05:32 +0100

    The actual fortran subroutine is now called prosail_fortran, so that it can be accessed directly from the package

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration(parent_package,top_path)
-    config.add_extension('prosail', ['prosail/dataSpec_P5B.f90', \
+    config.add_extension('prosail_fortran', ['prosail/dataSpec_P5B.f90', \
         "prosail/dladgen.f", "prosail/LIDF.f90", "prosail/MODULE_PRO4SAIL.f90", \
         "prosail/PRO4SAIL.f90", "prosail/prospect_5B.f90", \
         "prosail/run_prosail.f90","prosail/tav_abs.f90", \
-        "prosail/volscatt.f90","prosail/prosail.pyf"] )
+        "prosail/volscatt.f90","prosail/prosail_fortran.pyf"] )
     return config
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
     # Global variables for this extension:
-    name         = "prosail"  # name of the generated python extension (.so)
+    name         = "prosail_fortran"  # name of the generated python extension (.so)
     description  = "PRO4SAIL python wrappers"
     author       = "J Gomez-Dans/NCEO & University College London"
     author_email = "j.gomez-dans@ucl.ac.uk"
