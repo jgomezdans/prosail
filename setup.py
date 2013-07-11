e0291dd2755ae4d7fce49a5e2c7ff733d6a2978d
José Gómez-Dans <jgomezdans@gmail.com>
2013-07-11 17:51:52 +0100

    bumped up version number

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ if __name__ == "__main__":
         description=description, \
         author=author, \
         author_email = author_email, \
-        configuration = configuration, version="1.0.2",\
+        configuration = configuration, version="1.1.0",\
         packages=["prosail"])
