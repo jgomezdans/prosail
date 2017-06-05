5f8c57620bafd4615815ea58f65d7e9c35a028b7
Jose Gomez-Dans <jgomezdans@gmail.com>
2017-06-05 20:34:47 +0100

    Added thermal function to module interface

--- prosail/__init__.py
+++ prosail/__init__.py
@@ -2,7 +2,7 @@
 from spectral_library import get_spectra
 spectral_lib = get_spectra()
 from prospect_d import run_prospect
-from sail_model import run_sail, run_prosail
+from sail_model import run_sail, run_prosail, run_thermal_sail
 
    
 
