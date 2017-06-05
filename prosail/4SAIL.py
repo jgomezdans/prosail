60be4d272e963d644f6858f8b9dcd0256e8ad2cc
Jose Gomez-Dans <jgomezdans@gmail.com>
2017-06-05 19:15:58 +0100

    Cleaning up

--- prosail/4SAIL.py
+++ prosail/4SAIL.py
@@ -2,7 +2,8 @@
 from functools import lru_cache
 import numpy as np
 
-import numba
+
+from numba import autojit, jit
 
 
 def weighted_sum_over_lidf (lidf, tts, tto, psi, cts, cto, ctscto):
@@ -37,7 +38,7 @@ def weighted_sum_over_lidf (lidf, tts, tto, psi, cts, cto, ctscto):
         sof+=sofli*float(lidf[i])
     return ks, ko, bf, sob, sof
 
-cweighted_sum_over_lidf = numba.jit(weighted_sum_over_lidf)
+cweighted_sum_over_lidf =jit(weighted_sum_over_lidf)
 
 @lru_cache(maxsize=16)
 def define_geometric_constants ( tts, tto, psi):
@@ -75,9 +76,259 @@ def hotspot_calculations(alf, lai, ko, ks ):
 
     return tsstoo, sumint
 
-chotspot_calculations = numba.jit(hotspot_calculations)
+chotspot_calculations = jit(hotspot_calculations)
 
-def run_sail (rho, tau, lidfa, lidfb, lidftype, lai, hotspot, 
+
+@autojit
+def volscatt(tts,tto,psi,ttl) :
+    '''Compute volume scattering functions and interception coefficients
+    for given solar zenith, viewing zenith, azimuth and leaf inclination angle.
+    
+    Parameters
+    ----------
+    tts : float
+        Solar Zenith Angle (degrees).
+    tto : float
+        View Zenight Angle (degrees).
+    psi : float
+        View-Sun reliative azimuth angle (degrees).
+    ttl : float
+        leaf inclination angle (degrees).
+    
+    Returns
+    -------    
+    chi_s : float
+        Interception function  in the solar path.
+    chi_o : float
+        Interception function  in the view path.
+    frho : float
+        Function to be multiplied by leaf reflectance to obtain the volume scattering.
+    ftau : float
+        Function to be multiplied by leaf transmittance to obtain the volume scattering.
+    
+    References
+    ----------
+    Wout Verhoef, april 2001, for CROMA.
+    '''
+
+
+    cts = np.cos(np.radians(tts))
+    cto = np.cos(np.radians(tto))
+    sts = np.sin(np.radians(tts))
+    sto = np.sin(np.radians(tto))
+    cospsi = np.cos(np.radians(psi))
+    psir = np.radians(psi)
+    cttl = np.cos(np.radians(ttl))
+    sttl = np.sin(np.radians(ttl))
+    cs = cttl*cts
+    co = cttl*cto
+    ss = sttl*sts
+    so = sttl*sto  
+    cosbts = 5.
+    if np.abs(ss) > 1e-6 : 
+        cosbts = -cs/ss
+    cosbto = 5.
+    if np.abs(so) > 1e-6 : 
+        cosbto = -co/so
+    if np.abs(cosbts) < 1.0:
+        bts = np.acos(cosbts)
+        ds = ss
+    else:
+        bts = np.pi
+        ds = cs
+    chi_s = 2./np.pi*((bts-np.pi*0.5)*cs+np.sin(bts)*ss)
+    if abs(cosbto) < 1.0:
+        bto = np.acos(cosbto)
+        do_ = so
+    else:
+        if tto < 90.:
+            bto = np.pi
+            do_ = co
+        else:
+            bto = 0.0
+            do_ = -co
+    chi_o = 2.0/np.pi*((bto-np.pi*0.5)*co+np.sin(bto)*so)
+    btran1 = np.abs(bts-bto)
+    btran2 = np.pi-np.abs(bts+bto-np.pi)
+    if psir <= btran1:
+        bt1 = psir
+        bt2 = btran1
+        bt3 = btran2
+    else:
+        bt1 = btran1
+        if psir <= btran2:
+            bt2 = psir
+            bt3 = btran2
+        else:
+            bt2 = btran2
+            bt3 = psir
+    t1 = 2.*cs*co+ss*so*cospsi
+    t2 = 0.
+    if bt2 > 0.: 
+        t2 = np.sin(bt2)*(2.*ds*do_+ss*so*np.cos(bt1)*np.cos(bt3))
+    denom=2.*np.pi**2
+    frho = ((np.pi-bt2)*t1+t2)/denom
+    ftau = (-bt2*t1+t2)/denom
+    if frho < 0. : 
+        frho=0.
+    if ftau < 0. : 
+        ftau=0.
+   
+    return [chi_s,chi_o,frho,ftau]    
+
+@autojit
+def Jfunc1(k,l,t) :
+    ''' J1 function with avoidance of singularity problem.'''
+    nb=len(l)
+    del_=(k-l)*t
+    if nb > 1:
+        result=np.zeros(nb)
+        result[np.abs(del_) > 1e-3] = (np.exp(-l[np.abs(del_)> 1e-3]*t)-
+                                  np.exp(-k*t))/(k-l[np.abs(del_)> 1e-3])
+        result[np.abs(del_) <= 1e-3] = 0.5*t*(np.exp(-k*t) + 
+                                    np.exp(-l[np.abs(del_)<= 1e-3]*t))* \
+                                    (1.-(del_[np.abs(del_)<= 1e-3]**2.)/12.)
+    else:
+        if np.abs(del_) > 1e-3 :
+            result = (np.exp(-l*t)-np.exp(-k*t))/(k-l)
+        else:
+            result = 0.5*t*(np.exp(-k*t)+np.exp(-l*t))*(1.-(del_**2.)/12.)
+    return result
+
+@autojit
+def Jfunc2(k,l,t) :
+    '''J2 function.'''
+    return (1.-np.exp(-(k+l)*t))/(k+l)
+
+
+@autojit
+def verhoef_bimodal(a,b,n_elements=18):
+    '''Calculate the Leaf Inclination Distribution Function based on the 
+    Verhoef's bimodal LIDF distribution.
+    Parameters
+    ----------
+    a : float
+        controls the average leaf slope.
+    b : float
+        controls the distribution's bimodality.
+        
+            * LIDF type     [a,b].
+            * Planophile    [1,0].
+            * Erectophile   [-1,0].
+            * Plagiophile   [0,-1].
+            * Extremophile  [0,1].
+            * Spherical     [-0.35,-0.15].
+            * Uniform       [0,0].
+            * requirement: |LIDFa| + |LIDFb| < 1.	
+    n_elements : int
+        Total number of equally spaced inclination angles.
+    
+    Returns
+    -------
+    lidf : list
+        Leaf Inclination Distribution Function at equally spaced angles.
+    
+    References
+    ----------
+    .. [Verhoef1998] Verhoef, Wout. Theory of radiative transfer models applied 
+        in optical remote sensing of vegetation canopies. 
+        Nationaal Lucht en Ruimtevaartlaboratorium, 1998.
+        http://library.wur.nl/WebQuery/clc/945481.
+        '''
+
+    import math as m
+    freq=1.0
+    step=90.0/n_elements
+    lidf=[]
+    angles=[i*step for i in reversed(range(n_elements))]
+    for angle in angles:
+        tl1=m.radians(angle)
+        if a>1.0:
+            f = 1.0-m.cos(tl1)
+        else:
+            eps=1e-8
+            delx=1.0
+            x=2.0*tl1
+            p=float(x)
+            while delx >= eps:
+                y = a*m.sin(x)+.5*b*m.sin(2.*x)
+                dx=.5*(y-x+p)
+                x=x+dx
+                delx=abs(dx)
+            f = (2.*y+p)/m.pi
+        freq=freq-f
+        lidf.append(freq)
+        freq=float(f)
+    lidf=list(reversed(lidf))
+    return  lidf
+    
+@autojit
+def ellipsoidal(alpha,n_elements=18):
+    '''Calculate the Leaf Inclination Distribution Function based on the 
+    mean angle of [Campbell1990] ellipsoidal LIDF distribution.
+    Parameters
+    ----------
+    alpha : float
+        Mean leaf angle (degrees) use 57 for a spherical LIDF.
+    n_elements : int
+        Total number of equally spaced inclination angles .
+    
+    Returns
+    -------
+    lidf : list
+        Leaf Inclination Distribution Function for 18 equally spaced angles.
+        
+    References
+    ----------
+    .. [Campbell1986] G.S. Campbell, Extinction coefficients for radiation in 
+        plant canopies calculated using an ellipsoidal inclination angle distribution, 
+        Agricultural and Forest Meteorology, Volume 36, Issue 4, 1986, Pages 317-321, 
+        ISSN 0168-1923, http://dx.doi.org/10.1016/0168-1923(86)90010-9.
+    .. [Campbell1990] G.S Campbell, Derivation of an angle density function for 
+        canopies with ellipsoidal leaf angle distributions, 
+        Agricultural and Forest Meteorology, Volume 49, Issue 3, 1990, Pages 173-176, 
+        ISSN 0168-1923, http://dx.doi.org/10.1016/0168-1923(90)90030-A.
+    '''
+    
+    from math import cos, asin,tan, log, exp, sqrt, radians
+    
+    alpha=float(alpha)
+    excent=exp(-1.6184e-5*alpha**3.+2.1145e-3*alpha**2.-1.2390e-1*alpha+3.2491)
+    sum0 = 0.
+    freq=[]
+    step=90.0/n_elements
+    for  i in range (n_elements):
+        tl1=radians(i*step)
+        tl2=radians((i+1.)*step)
+        x1  = excent/(sqrt(1.+excent**2.*tan(tl1)**2.))
+        x2  = excent/(sqrt(1.+excent**2.*tan(tl2)**2.))
+        if excent == 1. :
+            freq.append(abs(cos(tl1)-cos(tl2)))
+        else :
+            alph  = excent/sqrt(abs(1.-excent**2.))
+            alph2 = alph**2.
+            x12 = x1**2.
+            x22 = x2**2.
+            if excent > 1. :
+                alpx1 = sqrt(alph2+x12)
+                alpx2 = sqrt(alph2+x22)
+                dum   = x1*alpx1+alph2*log(x1+alpx1)
+                freq.append(abs(dum-(x2*alpx2+alph2*log(x2+alpx2))))
+            else :
+                almx1 = sqrt(alph2-x12)
+                almx2 = sqrt(alph2-x22)
+                dum   = x1*almx1+alph2*asin(x1/alph)
+                freq.append(abs(dum-(x2*almx2+alph2*asin(x2/alph))))
+    sum0 = sum(freq)
+    lidf=[]
+    for i in range(n_elements):
+        lidf.append(float(freq[i])/sum0)
+    
+    return lidf
+
+
+
+def foursail (rho, tau, lidfa, lidfb, lidftype, lai, hotspot, 
     tts, tto, psi, rsoil):
     """
     Parameters
@@ -316,254 +567,3 @@ def run_sail (rho, tau, lidfa, lidfb, lidftype, lai, hotspot,
     return [tss,too,tsstoo,rdd,tdd,rsd,tsd,rdo,tdo,
           rso,rsos,rsod,rddt,rsdt,rdot,rsodt,rsost,rsot,gammasdf,gammasdb,gammaso]
 
-
-def volscatt(tts,tto,psi,ttl) :
-    '''Compute volume scattering functions and interception coefficients
-    for given solar zenith, viewing zenith, azimuth and leaf inclination angle.
-    
-    Parameters
-    ----------
-    tts : float
-        Solar Zenith Angle (degrees).
-    tto : float
-        View Zenight Angle (degrees).
-    psi : float
-        View-Sun reliative azimuth angle (degrees).
-    ttl : float
-        leaf inclination angle (degrees).
-    
-    Returns
-    -------    
-    chi_s : float
-        Interception function  in the solar path.
-    chi_o : float
-        Interception function  in the view path.
-    frho : float
-        Function to be multiplied by leaf reflectance to obtain the volume scattering.
-    ftau : float
-        Function to be multiplied by leaf transmittance to obtain the volume scattering.
-    
-    References
-    ----------
-    Wout Verhoef, april 2001, for CROMA.
-    '''
-
-    from math import sin, cos, acos, radians, pi 
-    cts=cos(radians(tts))
-    cto=cos(radians(tto))
-    sts=sin(radians(tts))
-    sto=sin(radians(tto))
-    cospsi=cos(radians(psi))
-    psir=radians(psi)
-    cttl=cos(radians(ttl))
-    sttl=sin(radians(ttl))
-    cs=cttl*cts
-    co=cttl*cto
-    ss=sttl*sts
-    so=sttl*sto  
-    cosbts=5.
-    if abs(ss) > 1e-6 : cosbts=-cs/ss
-    cosbto=5.
-    if abs(so) > 1e-6 : cosbto=-co/so
-    if abs(cosbts) < 1.0:
-        bts=acos(cosbts)
-        ds=ss
-    else:
-        bts=pi
-        ds=cs
-    chi_s=2./pi*((bts-pi*0.5)*cs+sin(bts)*ss)
-    if abs(cosbto) < 1.0:
-        bto=acos(cosbto)
-        do_=so
-    else:
-        if tto < 90.:
-            bto=pi
-            do_=co
-        else:
-            bto=0.0
-            do_=-co
-    chi_o=2.0/pi*((bto-pi*0.5)*co+sin(bto)*so)
-    btran1=abs(bts-bto)
-    btran2=pi-abs(bts+bto-pi)
-    if psir <= btran1:
-        bt1=psir
-        bt2=btran1
-        bt3=btran2
-    else:
-        bt1=btran1
-        if psir <= btran2:
-            bt2=psir
-            bt3=btran2
-        else:
-            bt2=btran2
-            bt3=psir
-    t1=2.*cs*co+ss*so*cospsi
-    t2=0.
-    if bt2 > 0.: t2=sin(bt2)*(2.*ds*do_+ss*so*cos(bt1)*cos(bt3))
-    denom=2.*pi**2
-    frho=((pi-bt2)*t1+t2)/denom
-    ftau=(-bt2*t1+t2)/denom
-    if frho < 0. : frho=0.
-    if ftau < 0. : ftau=0.
-   
-    return [chi_s,chi_o,frho,ftau]    
-
-def Jfunc1(k,l,t) :
-    ''' J1 function with avoidance of singularity problem.'''
-    from numpy import exp,zeros,size
-    nb=size(l)
-    del_=(k-l)*t
-    if nb > 1:
-        result=zeros(nb)
-        result[abs(del_) > 1e-3]=(exp(-l[abs(del_)> 1e-3]*t)-exp(-k*t))/(k-l[abs(del_)> 1e-3])
-        result[abs(del_)<= 1e-3]=0.5*t*(exp(-k*t)+exp(-l[abs(del_)<= 1e-3]*t))*(1.-(del_[abs(del_)<= 1e-3]**2.)/12.)
-    else:
-        if abs(del_) > 1e-3 :
-            result=(exp(-l*t)-exp(-k*t))/(k-l)
-        else:
-            result=0.5*t*(exp(-k*t)+exp(-l*t))*(1.-(del_**2.)/12.)
-    return result
-
-def Jfunc2(k,l,t) :
-    '''J2 function.'''
-    from numpy import exp
-    return (1.-exp(-(k+l)*t))/(k+l)
-
-def Jfunc1_wl(k,l,t) :
-    '''J1 function with avoidance of singularity problem.'''
-    from math import exp
-    del_=(k-l)*t
-    if abs(del_) > 1e-3 :
-      result=(exp(-l*t)-exp(-k*t))/(k-l)
-    else:
-      result=0.5*t*(exp(-k*t)+exp(-l*t))*(1.-(del_**2.)/12.)
-    return result
-
-def Jfunc2_wl(k,l,t) :
-    '''J2 function.'''
-    from math import exp
-    return (1.-exp(-(k+l)*t))/(k+l)
-
-
-def verhoef_bimodal(a,b,n_elements=18):
-    '''Calculate the Leaf Inclination Distribution Function based on the 
-    Verhoef's bimodal LIDF distribution.
-    Parameters
-    ----------
-    a : float
-        controls the average leaf slope.
-    b : float
-        controls the distribution's bimodality.
-        
-            * LIDF type     [a,b].
-            * Planophile    [1,0].
-            * Erectophile   [-1,0].
-            * Plagiophile   [0,-1].
-            * Extremophile  [0,1].
-            * Spherical     [-0.35,-0.15].
-            * Uniform       [0,0].
-            * requirement: |LIDFa| + |LIDFb| < 1.	
-    n_elements : int
-        Total number of equally spaced inclination angles.
-    
-    Returns
-    -------
-    lidf : list
-        Leaf Inclination Distribution Function at equally spaced angles.
-    
-    References
-    ----------
-    .. [Verhoef1998] Verhoef, Wout. Theory of radiative transfer models applied 
-        in optical remote sensing of vegetation canopies. 
-        Nationaal Lucht en Ruimtevaartlaboratorium, 1998.
-        http://library.wur.nl/WebQuery/clc/945481.
-        '''
-
-    import math as m
-    freq=1.0
-    step=90.0/n_elements
-    lidf=[]
-    angles=[i*step for i in reversed(range(n_elements))]
-    for angle in angles:
-        tl1=m.radians(angle)
-        if a>1.0:
-            f = 1.0-m.cos(tl1)
-        else:
-            eps=1e-8
-            delx=1.0
-            x=2.0*tl1
-            p=float(x)
-            while delx >= eps:
-                y = a*m.sin(x)+.5*b*m.sin(2.*x)
-                dx=.5*(y-x+p)
-                x=x+dx
-                delx=abs(dx)
-            f = (2.*y+p)/m.pi
-        freq=freq-f
-        lidf.append(freq)
-        freq=float(f)
-    lidf=list(reversed(lidf))
-    return  lidf
-    
-def ellipsoidal(alpha,n_elements=18):
-    '''Calculate the Leaf Inclination Distribution Function based on the 
-    mean angle of [Campbell1990] ellipsoidal LIDF distribution.
-    Parameters
-    ----------
-    alpha : float
-        Mean leaf angle (degrees) use 57 for a spherical LIDF.
-    n_elements : int
-        Total number of equally spaced inclination angles .
-    
-    Returns
-    -------
-    lidf : list
-        Leaf Inclination Distribution Function for 18 equally spaced angles.
-        
-    References
-    ----------
-    .. [Campbell1986] G.S. Campbell, Extinction coefficients for radiation in 
-        plant canopies calculated using an ellipsoidal inclination angle distribution, 
-        Agricultural and Forest Meteorology, Volume 36, Issue 4, 1986, Pages 317-321, 
-        ISSN 0168-1923, http://dx.doi.org/10.1016/0168-1923(86)90010-9.
-    .. [Campbell1990] G.S Campbell, Derivation of an angle density function for 
-        canopies with ellipsoidal leaf angle distributions, 
-        Agricultural and Forest Meteorology, Volume 49, Issue 3, 1990, Pages 173-176, 
-        ISSN 0168-1923, http://dx.doi.org/10.1016/0168-1923(90)90030-A.
-    '''
-    
-    from math import cos, asin,tan, log, exp, sqrt, radians
-    
-    alpha=float(alpha)
-    excent=exp(-1.6184e-5*alpha**3.+2.1145e-3*alpha**2.-1.2390e-1*alpha+3.2491)
-    sum0 = 0.
-    freq=[]
-    step=90.0/n_elements
-    for  i in range (n_elements):
-        tl1=radians(i*step)
-        tl2=radians((i+1.)*step)
-        x1  = excent/(sqrt(1.+excent**2.*tan(tl1)**2.))
-        x2  = excent/(sqrt(1.+excent**2.*tan(tl2)**2.))
-        if excent == 1. :
-            freq.append(abs(cos(tl1)-cos(tl2)))
-        else :
-            alph  = excent/sqrt(abs(1.-excent**2.))
-            alph2 = alph**2.
-            x12 = x1**2.
-            x22 = x2**2.
-            if excent > 1. :
-                alpx1 = sqrt(alph2+x12)
-                alpx2 = sqrt(alph2+x22)
-                dum   = x1*alpx1+alph2*log(x1+alpx1)
-                freq.append(abs(dum-(x2*alpx2+alph2*log(x2+alpx2))))
-            else :
-                almx1 = sqrt(alph2-x12)
-                almx2 = sqrt(alph2-x22)
-                dum   = x1*almx1+alph2*asin(x1/alph)
-                freq.append(abs(dum-(x2*almx2+alph2*asin(x2/alph))))
-    sum0 = sum(freq)
-    lidf=[]
-    for i in range(n_elements):
-        lidf.append(float(freq[i])/sum0)
-    
-    return lidf
