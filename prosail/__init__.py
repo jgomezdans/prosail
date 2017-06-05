00b5890b3f0e596387afc38aa37b0ce36542cf26
Jose Gomez-Dans <jgomezdans@gmail.com>
2017-06-05 19:10:42 +0100

    Put spectral stuff on its own file

--- prosail/__init__.py
+++ prosail/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from collections import namedtuple
-import pkgutil
-from StringIO import StringIO
 
 import numpy as np
+
+from spectral_libary import get_spectra
 from prospect_d import run_prospect
 
 Spectra = namedtuple('Spectra', 'prospect5 prospectd soil light')
@@ -14,41 +14,9 @@ ProspectDSpectra = namedtuple('ProspectDSpectra',
 SoilSpectra = namedtuple("SoilSpectra", "rsoil1 rsoil2")
 LightSpectra = namedtuple("LightSpectra", "es ed")
 
-spectral_lib = get_spectra()
 
-def get_spectra():
-    """Reads the spectral information and stores is for future use."""
-
-    # PROSPECT-D
-    prospect_d_spectraf = pkgutil.get_data('prosail', 'prospect_d_spectra.txt')
-    d = np.loadtxt( StringIO(prospect_d_spectraf))
-    nr = d[:, 1]
-    kab = d[:, 2]
-    kcar = d[:, 3]
-    kbrown = d[:, 5]
-    kw = d[:, 6]
-    km = d[:, 7]
-    kant = d[:, 4]
-    prospect_d_spectra = ProspectDSpectra(nr, kab, kcar, kbrown, kw, km, kant)
-    # PROSPECT 5
-    prospect_5_spectraf = pkgutil.get_data('prosail', 'prospect5_spectra.txt')
-    nr, kab, kcar, kbrown, kw, km =  np.loadtxt(StringIO(prospect_5_spectraf), 
-                                                unpack=True)
-    prospect_5_spectra = Prospect5Spectra(nr, kab, kcar, kbrown, kw, km)
-    # SOIL
-    soil_spectraf = pkgutil.get_data('prosail', 'soil_reflectance.txt')
-    rsoil1, rsoil2 =  np.loadtxt(StringIO(soil_spectraf), 
-                                                unpack=True)
-    soil_spectra = SoilSpectra(rsoil1, rsoil2)    
-    # LIGHT
-    light_spectraf = pkgutil.get_data('prosail', 'light_spectra.txt')
-    es, ed =  np.loadtxt(StringIO(light_spectraf), 
-                                                unpack=True)
-    light_spectra = LightSpectra(es, ed)
-    spectra = Spectra(prospect_d_spectra, prospect_5_spectra, 
-                      soil_spectra, light_spectra)
-    return spectra
     
+spectral_lib = get_spectra()
     
     
 
---
+++ prosail/spectral_library.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""Spectral libraries for PROSPECT + SAIL
+"""
+from collections import namedtuple
+import pkgutil
+from StringIO import StringIO
+
+import numpy as np
+
+def get_spectra():
+    """Reads the spectral information and stores is for future use."""
+
+    # PROSPECT-D
+    prospect_d_spectraf = pkgutil.get_data('prosail', 'prospect_d_spectra.txt')
+    d = np.loadtxt( StringIO(prospect_d_spectraf))
+    nr = d[:, 1]
+    kab = d[:, 2]
+    kcar = d[:, 3]
+    kbrown = d[:, 5]
+    kw = d[:, 6]
+    km = d[:, 7]
+    kant = d[:, 4]
+    prospect_d_spectra = ProspectDSpectra(nr, kab, kcar, kbrown, kw, km, kant)
+    # PROSPECT 5
+    prospect_5_spectraf = pkgutil.get_data('prosail', 'prospect5_spectra.txt')
+    nr, kab, kcar, kbrown, kw, km =  np.loadtxt(StringIO(prospect_5_spectraf), 
+                                                unpack=True)
+    prospect_5_spectra = Prospect5Spectra(nr, kab, kcar, kbrown, kw, km)
+    # SOIL
+    soil_spectraf = pkgutil.get_data('prosail', 'soil_reflectance.txt')
+    rsoil1, rsoil2 =  np.loadtxt(StringIO(soil_spectraf), 
+                                                unpack=True)
+    soil_spectra = SoilSpectra(rsoil1, rsoil2)    
+    # LIGHT
+    light_spectraf = pkgutil.get_data('prosail', 'light_spectra.txt')
+    es, ed =  np.loadtxt(StringIO(light_spectraf), 
+                                                unpack=True)
+    light_spectra = LightSpectra(es, ed)
+    spectra = Spectra(prospect_d_spectra, prospect_5_spectra, 
+                      soil_spectra, light_spectra)
+    return spectra
