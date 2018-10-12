58d99dcae68bd89d76df917729a34de332474afb
Jose Gomez-Dans <jgomezdans@gmail.com>
2018-10-12 16:06:35 +0100

    Ensure that PROSPECT spectra are stored in library
    I think the PROSPECT spectra are now distributed with the library.
    Addresses #11, @MarcYin can you please test? Thanks!

--- prosail/__init__.py
+++ prosail/__init__.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+__author__ = "J Gomez-Dans"
+__copyright__ = "Copyright 2017, 2018 J Gomez-Dans"
+__version__ = "2.0.2"
+__license__ = "GPLv3"
+__email__ = "j.gomez-dans@ucl.ac.uk"
+
 from .spectral_library import get_spectra
 spectral_lib = get_spectra()
 from .prospect_d import run_prospect
--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python
 """Setup script for building prosail's python bindings"""
-from setuptools import setup
 import os
+import codecs
+import re
+from os import path
+from setuptools import setup
+
 # Global variables for this extension:
 name = "prosail"  # name of the generated python extension (.so)
 description = "PROSPECT, SAIL and PROSAIL Python wrappers"
 long_description = "The PROSPECT + SAILh radiative transfer models from Python"
 
+this_directory = path.abspath(path.dirname(__file__))                                                              
 
 def read(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename), "rb") as f:
+    with open(os.path.join(this_directory, filename), "rb") as f:
         return f.read().decode("utf-8")
 
 
 if os.path.exists("README.md"):
     long_description = read("README.md")
+
+def read(*parts):                                                                                                  
+    with codecs.open(os.path.join(this_directory, *parts), 'r') as fp:                                             
+        return fp.read()                                                                                           
+                                                                                                                   
+def find_version(*file_paths):                                                                                     
+    version_file = read(*file_paths)                                                                               
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",                                               
+                              version_file, re.M)                                                                  
+    if version_match:                                                                                              
+        return version_match.group(1)                                                                              
+    raise RuntimeError("Unable to find version string.")
+
 
 author = "J Gomez-Dans/NCEO & University College London"
 author_email = "j.gomez-dans@ucl.ac.uk"
@@ -35,6 +53,9 @@ classifiers = [
     'Environment :: Console'
 ]
 
+
+
+
 setup(
     name=name,
     description=description,
@@ -52,7 +73,7 @@ setup(
         "scipy",
         "pytest",
     ],
-    version="2.0.1-1",
+    version=find_version("prosail", "__init__.py"),
     packages=["prosail"],
     zip_safe=False # Apparently needed for conda
 )
