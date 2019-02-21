b26062dd681a371030fc5b21202d0335a8d1f45f
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-02-21 18:13:08 +0000

    Bumped version to 2.0.4

--- prosail/__init__.py
+++ prosail/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 __author__ = "J Gomez-Dans"
 __copyright__ = "Copyright 2017, 2018 J Gomez-Dans"
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 __license__ = "GPLv3"
 __email__ = "j.gomez-dans@ucl.ac.uk"
 
